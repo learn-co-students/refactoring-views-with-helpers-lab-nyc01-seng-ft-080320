063285e1a40928eede524eb5b0869d9bb74f4b46
AmSmo <adam.smolenski@gmail.com>
2020-09-04 13:23:16 -0400

    how to return html

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,8 @@
 module ArtistsHelper
+    def display_artist(song)
+        if !song.artist.present?
+            
+            "<%= link_to 'Add Artist', #{edit_song_path}(#{song.id}) %>"
+        end
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      self.artist.name
+    else
+      "Unknown Artist"
+    end
   end
 
   def artist_name=(name)
